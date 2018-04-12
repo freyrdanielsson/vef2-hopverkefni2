17289415cfffd15d638ed6425d06ec45c66fa8e1
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-04-12 23:13:22 +0000

    basic login with mock function

--- src/api.js
+++ src/api.js
@@ -18,8 +18,38 @@ async function get(endpoint) {
   /* todo framkvæma get */
 }
 
+
+/*--------------------------------------*/
+// Mock fall, þarf að skipta út fyrir alvöru kall í vefþjónustu
+
+function login(username, password) {
+  return new Promise((resolve, reject) => {
+    const user = {
+      name: 'Mock admin user',
+      username: 'admin',
+    }
+
+    if (username === 'error') {
+      return reject('Villa');
+    }
+
+    if (username === 'admin' && password === '123') {
+      return setTimeout(() => resolve({ loggedin: true, user }), 1000);
+    }
+
+    if (username !== 'admin') {
+      return setTimeout(() => resolve({ loggedin: false, error: 'Notandi ekki til' }), 500);
+    }
+
+    return setTimeout(() => resolve({ loggedin: false, error: 'Vitlaust lykilorð' }), 500);
+  });
+}
+
+/*--------------------------------------*/
+
 /* todo aðrar aðgerðir */
 
 export default {
   get,
+  login
 };
