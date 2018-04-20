25ea4f28b313518c74370a8308e8c3815a05de8a
arnar44 <arnar44@gmail.com>
2018-04-20 22:45:54 +0000

    laga varning í getpramas() og helmet á forsíðu

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Helmet from 'react-helmet';
 
 import './Home.css';
 
@@ -12,23 +13,24 @@ class Home extends Component {
 
     return (
       <section className="home">
-      <h1 className="welcome">Velkomin á bókasafnið</h1>
-      <div>
-        {authenticated 
-        ? <div><p>Þú ert skráður notandi og getur því &nbsp;
-          <Link to="/books/new">skráð bækur</Link> &nbsp;
-          og breytt &nbsp;
-          <Link to="/books">þeim sem til eru</Link>.&nbsp;
-          </p><p> Einnig getur þú &nbsp;
-          <Link to="/users">skoðað aðra notendur</Link>.&nbsp;
-        </p></div>
-        : <p>Til að njóta bókasafnsins til fullnustu mælum við með að &nbsp;
-          <Link to="/login">skrá sig inn</Link>. &nbsp;
-          Þangað til getur þú skoðað &nbsp;
-          <Link to="/books">allar bækur</Link>.
-          </p>}
+      <Helmet title="Forsíða" />
+        <h1 className="welcome">Velkomin á bókasafnið</h1>
+        <div>
+          {authenticated 
+            ? <div><p>Þú ert skráður notandi og getur því &nbsp;
+              <Link to="/books/new">skráð bækur</Link> &nbsp;
+              og breytt &nbsp;
+              <Link to="/books">þeim sem til eru</Link>.&nbsp;
+              </p><p> Einnig getur þú &nbsp;
+              <Link to="/users">skoðað aðra notendur</Link>.&nbsp;
+              </p></div>
+            : <p>Til að njóta bókasafnsins til fullnustu mælum við með að &nbsp;
+              <Link to="/login">skrá sig inn</Link>. &nbsp;
+              Þangað til getur þú skoðað &nbsp;
+              <Link to="/books">allar bækur</Link>.
+              </p>}
       </div>
-      </section>
+    </section>
     );
   }
 }
